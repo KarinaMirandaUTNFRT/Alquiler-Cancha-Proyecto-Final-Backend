f5f0bc98a185fe446eedce5a01a70783577d0a4b
Karina Miranda <Karina.Miranda@frt.utn.edu.ar>
2026-08-29 10:55:14 -0300

    se creo el modelo cancha

---
+++ src/models/cancha.js
@@ -0,0 +1,38 @@
+import mongoose, { Schema } from "mongoose";
+
+const canchaSchema = new Schema(
+    {
+        nombreCancha:
+           nombreCancha:{type: String,
+             required: true,
+             unique: true,
+             minlength:5, 
+             maxlength:100,
+             trim: true,
+                },
+    precio:{type: Number, 
+        required: true, 
+        min: 50, 
+    },
+    imagen:{ 
+        type: String, 
+        required: true, 
+        validate: (valor) => /^https:\/\/.+\.(jpg|jpeg|png|webp|avif|svg)$/.test(valor),
+    },
+    categoria:{
+        type: Schema.Types.ObjectId, 
+        required: true,     
+    ref: "categoria",
+},
+    descripcion: { type: String,  
+        minlength: 10, 
+        maxlength:500, 
+        required: true,
+    },
+    
+    {
+    timpestamps: true
+}
+);
+const Cancha = mongoose.modelo('cancha', canchaSchema) 
+export default Cancha
