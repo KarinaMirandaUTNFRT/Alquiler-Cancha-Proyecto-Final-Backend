cb4d39e1dc61f6d3526c09af507bd3dce4e26225
Karina Miranda <Karina.Miranda@frt.utn.edu.ar>
2026-08-29 19:50:48 -0300

    Ajustes de configuracion estructura de proyecto

--- src/routes/index.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router  } from "express";
-import canchasRouter from "./cancha.routes.js"
-import CategoriaCanchasRouter from "./categoriasCanchas.routes.js"
-
-const router =Router()
-router.use("/canchas", canchasRouter);
-router.use("/categoriaCanchas", CategoriaCanchasRouter);
-
-export default router;
