d88547a5aab219abb6dd73140561dff089501786
Nair Paez <nairpaez@outlook.com>
2026-08-29 20:19:26 -0300

    modificaos index rputes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -3,8 +3,7 @@ import canchasRouter from "./cancha.routes.js"
 import CategoriaCanchasRouter from "./categoriasCanchas.routes.js"
 import productosRoutes from './productos.routes.js'
 import categoriaProductosRoutes from './categoriasProductos.routes.js'
-import canchasRouter from "./cancha.routes.js"
-import CategoriaCanchasRouter from "./categoriasCanchas.routes.js"
+
 
 const router =Router()
 router.use("/canchas", canchasRouter);
