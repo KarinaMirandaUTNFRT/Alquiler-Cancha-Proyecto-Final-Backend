8ed6a8b84563964da9785a541d539095271b2ad7
Nair Paez <nairpaez@outlook.com>
2026-08-29 19:08:42 -0300

    creamos en productoController las funcioness obtener - eliminar y editar un producto por ID

--- src/controllers/producto.controllers.js
+++ src/controllers/producto.controllers.js
@@ -24,3 +24,59 @@ export const listarProductos = async (req, res) => {
       .json({ mensaje: "Ocurrio un error al listar los producto" });
   }
 };
+
+export const obtenerProductoPorID = async (req, res) => {
+  try {
+    console.log(req.params.id);
+    const productoBuscado = await Producto.findById(req.params.id).populate('categoria', 'nombre descripcion');
+    console.log(productoBuscado)
+    if (!productoBuscado) {
+      return res
+        .status(404)
+        .json({ mensaje: "No se encontro un producto con ese ID" });
+    }
+    res.status(200).json(productoBuscado);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ mensaje: "Ocurrio un error al buscar un producto por ID" });
+  }
+};
+
+
+export const borrarProductoPorID = async (req, res) => {
+  try {
+    const productoBorrado = await Productoroducto.findByIdAndDelete(req.params.id);
+   
+    if (!productoBorrado) {
+      return res
+        .status(404)
+        .json({ mensaje: "No se encontro un producto con ese ID" });
+    }
+    res.status(200).json({mensaje: 'El producto fue borrado correctamente'});
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ mensaje: "Ocurrio un error al intentar borrar un producto por ID" });
+  }
+};
+
+export const editarProductoPorID = async (req, res) => {
+  try {
+    //deberia validar que el id exista y sea un id de mongodb
+    const productoActualizado = await producto.findByIdAndUpdate(req.params.id, req.body, {new:true})
+    if (!productoActualizado) {
+      return res
+        .status(404)
+        .json({ mensaje: "No se encontro un producto con el id enviado" });
+    }
+    res.status(200).json({mensaje: 'El producto fue editado correctamente', producto: productoActualizado});
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ mensaje: "Ocurrio un error al intentar editar un producto por id" });
+  }
+};
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { crearProducto, listarProductos } from "../controllers/producto.controllers.js";
+import { borrarProductoPorID, crearProducto, editarProductoPorID, listarProductos, obtenerProductoPorID } from "../controllers/producto.controllers.js";
 
 const router = Router()
 
 router.route('/').post(crearProducto).get(listarProductos)
+router.route('/:id').get(obtenerProductoPorID).delete(borrarProductoPorID).put(editarProductoPorID).patch(editarProductoPorID)
