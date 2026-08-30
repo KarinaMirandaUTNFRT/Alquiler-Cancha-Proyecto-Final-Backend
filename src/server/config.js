8ef2ea1a67510bc860c27f8fff973dd3ae52a3f7
Nair Paez <nairpaez@outlook.com>
2026-08-30 16:34:44 -0300

    resolvimos probleba en archivo config.js

--- src/server/config.js
+++ src/server/config.js
@@ -4,7 +4,6 @@ import morgan from "morgan";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import '../database/db.js'
-import categoriaCanchaRoutes from '../routes/categoriasCanchas.routes.js'
 
 export default class Server {
   // el objetivo del construcotr es definir las propiedad el futuro objeto
@@ -12,11 +11,6 @@ export default class Server {
     this.app = express();
     this.PORT = process.env.PORT || 3000;
     this.middlewares()
-    this.routes();
-  }
-  routes(){
-    this.app.use('/categoriaCanchas', categoriaCanchaRoutes)
-    this.app.use('/canchas', categoriaCanchaRoutes)
   }
 
   // aqui agregar el resto de los metodos del futuro objeto
@@ -30,9 +24,6 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // cargar el siguiente archivo en forma estatica
 this.app.use(express.static(__dirname + '/../../public'))
-  }
-route() {
-    this.app.use("/api", router);
   }
 
   listen() {
