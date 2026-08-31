6b551c5418892393ad1772cc68fec6b7b1bf5b27
Nair Paez <nairpaez@outlook.com>
2026-08-31 10:43:36 -0300

    eliminamos linea de mas en resiltValidacion

--- src/middlewares/resultadoValidacion.js
+++ src/middlewares/resultadoValidacion.js
@@ -1,13 +1,12 @@
 import { validationResult } from "express-validator";
 
-const resultadoValidacion  = (req, res, next) => {
- const errores = validationResult(req)
- console.log(errores.isEmpty())
- if (!errores.isEmpty()){
-    return res.status(400).json(errores.array())
- }
- // continuar con el siguiente codigo
- next()
-}
+const resultadoValidacion = (req, res, next) => {
+  const errores = validationResult(req);
+  if (!errores.isEmpty()) {
+    return res.status(400).json(errores.array());
+  }
+  // continuar con el siguiente codigo
+  next();
+};
 
-export default resultadoValidacion
+export default resultadoValidacion;
