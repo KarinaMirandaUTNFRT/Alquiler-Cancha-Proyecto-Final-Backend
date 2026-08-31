b6df0e528405cf5efcd8c950b2a47df589e0a7ab
Karina Miranda <Karina.Miranda@frt.utn.edu.ar>
2026-08-31 10:42:03 -0300

    se elimina el console.log

--- src/middlewares/resultadoValidacion.js
+++ src/middlewares/resultadoValidacion.js
@@ -2,8 +2,7 @@ import { validationResult } from "express-validator";
 
 const resultadoValidacion =(req, res, next) => {
    const errores = validationResult(req)
-   console.log(errores.isEmpty())
-
+  
    if(!errores.isEmpty()){
     return res.status(400).json(errores.array())
    }
