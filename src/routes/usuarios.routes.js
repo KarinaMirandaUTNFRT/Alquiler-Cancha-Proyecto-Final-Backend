3b3687cf17328a6df0637b1cbce4d30d90d558d3
Karina Miranda <Karina.Miranda@frt.utn.edu.ar>
2026-09-01 12:44:19 -0300

    se redordena codigo

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,11 +1,25 @@
 import { Router } from "express";
-import { borrarUsuarioPorID, crearUsuario, editarUsuarioPorID, listarUsuarios, obtenerUsuariosPorID } from "../controllers/usuarios.controllers.js";
-import { validacionIDUsuario, validacionUsuario, validacionUsuarioPatch } from "../middlewares/validacionUsuario.js";
+import {
+   borrarUsuarioPorID, 
+   crearUsuario, 
+   editarUsuarioPorID, 
+   listarUsuarios,
+   obtenerUsuariosPorID 
+  } from "../controllers/usuarios.controllers.js";
+import { validacionIDUsuario, 
+  validacionUsuario,
+  validacionUsuarioPatch } from "../middlewares/validacionUsuario.js";
 
 const router = Router()
-// http://localhost:3000/api/usuarios/
 
-router.route('/').post(validacionUsuario,crearUsuario).get(listarUsuarios)
-router.route('/:id').get(validacionIDUsuario,obtenerUsuariosPorID).delete(validacionIDUsuario, borrarUsuarioPorID).put([validacionIDUsuario,validacionUsuario],editarUsuarioPorID).patch(validacionUsuarioPatch,editarUsuarioPorID)
+router.route('/')
+.post(validacionUsuario,crearUsuario)
+.get(listarUsuarios)
+
+router.route('/:id')
+.get(validacionIDUsuario,obtenerUsuariosPorID)
+.delete(validacionIDUsuario, borrarUsuarioPorID)
+.put([validacionIDUsuario,validacionUsuario],editarUsuarioPorID)
+.patch(validacionUsuarioPatch,editarUsuarioPorID)
 
 export default router
