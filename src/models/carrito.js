a5ca427e56ac460fe625420cf149fec77ea0e143
Nair Paez <nairpaez@outlook.com>
2026-09-03 11:03:14 -0300

    creamos modelo de carrito

---
+++ src/models/carrito.js
@@ -0,0 +1,43 @@
+import mongoose, { Schema } from "mongoose";
+
+const carritoSchema = new Schema(
+  {
+    usuario: {
+      type: Schema.Types.ObjectId,
+      ref: "usuario",
+      required: true,
+      unique: true,
+    },
+    items: [
+      {
+        cancha: {
+          type: Schema.Types.ObjectId,
+          ref: "cancha",
+          required: true,
+        },
+        cantidadCancha: {
+          type: Number,
+          default: 1,
+          min: 1,
+        },
+        producto: {
+          type: Schema.Types.ObjectId,
+          ref: "producto",
+          required: true,
+        },
+        cantidadProducto: {
+          type: Number,
+          default: 1,
+          min: 1, .
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Carrito = mongoose.model('carrito', carritoSchema)
+
+export default Carrito
