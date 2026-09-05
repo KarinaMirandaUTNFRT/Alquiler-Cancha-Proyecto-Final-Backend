dd0a51b42db2af74d5f85ea81015833d92d2a1c4
Nair Paez <nairpaez@outlook.com>
2026-09-05 19:19:47 -0300

    agregamos configracion de Cors en config.js

--- src/server/config.js
+++ src/server/config.js
@@ -15,7 +15,14 @@ export default class Server {
   }
   
   middlewares(){
-    this.app.use(cors()); 
+    this.app.use(cors(
+      {
+        origin: process.env.FRONTEND_URL ||"http://localhost:5173",
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"]
+      }
+    )); 
 this.app.use(express.json()); 
 this.app.use(morgan("dev"));
 this.app.use(cookieParser())
