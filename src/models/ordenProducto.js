c2101ff399cb8e725e0ee2d052d3bc1dc55758b7
Nair Paez <nairpaez@outlook.com>
2026-09-06 16:05:27 -0300

    editamos modelos ordenProducto

--- src/models/ordenProducto.js
+++ src/models/ordenProducto.js
@@ -11,7 +11,7 @@ const ordenProductoSchema = new Schema(
       {
         producto: {
           type: Schema.Types.ObjectId,
-          ref: "servicio",
+          ref: "producto",
           required: true,
         },
         nombreProducto:{
