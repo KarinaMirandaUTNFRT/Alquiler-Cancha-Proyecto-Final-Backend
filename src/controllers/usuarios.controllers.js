c1376a3dd66f2844619d25f59c207e0e852c2746
Karina Miranda <Karina.Miranda@frt.utn.edu.ar>
2026-09-15 18:01:51 -0300

    se cambi nuevamente el nombre

--- src/controllers/usuarios.controllers.js
+++ src/controllers/usuarios.controllers.js
@@ -226,7 +226,7 @@ export const login = async (req, res) => {
       .status(200)
       .json({ 
         _id: usuarioBuscado._id,       // 👈 El ObjectId de Mongo Atlas
-      nombreUsuario: usuarioBuscado.nombre,
+      nombre:usuarioBuscado.nombreUsuario,
       email: usuarioBuscado.email,
       rol: usuarioBuscado.rol,
       });
