294e19df754fa7fcc1d23642c7bd75a88a564ab7
Karina Miranda <Karina.Miranda@frt.utn.edu.ar>
2026-09-15 22:56:52 -0300

    se cambia la direccion deployada

--- src/server/config.js
+++ src/server/config.js
@@ -17,7 +17,7 @@ export default class Server {
   middlewares() {
     this.app.use(
       cors({
-        origin: process.env.FRONTEND_URL || "http://localhost:5173",
+        origin: process.env.FRONTEND_URL || "https://rollingclub-frontend.netlify.app",
         credentials: true,
         methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
         allowedHeaders: ["Content-Type", "Authorization"],
