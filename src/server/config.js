ea9edab82b6f4ad8caaaeecc50a706d44544f84f
Karina Miranda <Karina.Miranda@frt.utn.edu.ar>
2026-09-16 17:56:09 -0300

    se cambia direccion de FRONTEND_URL

--- src/server/config.js
+++ src/server/config.js
@@ -17,7 +17,7 @@ export default class Server {
   middlewares() {
     this.app.use(
       cors({
-        origin: process.env.FRONTEND_URL || "https://rollingclubfront.netlify.app",
+        origin: process.env.FRONTEND_URL || "https://rollingclub.netlify.app/",
         credentials: true,
         methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
         allowedHeaders: ["Content-Type", "Authorization"],
